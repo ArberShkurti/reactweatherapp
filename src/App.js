5d3564394d8906e65c025fd5437b7baf1f22fdf2
Arber Shkurti <arbershkurti@yahoo.com>
2023-04-23 19:42:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FirtsPage from "./components/FirtsPage";
+
+function App() {
+  return (
+    <div className="App">
+      <FirtsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirtsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const FirtsPage =() => {
+
+    return (
+        <p>The fist text</p>
+    )
+
+}
+
+
+export default FirtsPage;
