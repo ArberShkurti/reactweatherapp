9b394672d4c7fdd87caceb5f2f51e488570d4199
Arber Shkurti <arbershkurti@yahoo.com>
2024-03-31 00:07:07 +0100

    mtt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Navbar = () => {
+
   return (
     <div>
         <ul>
             <li>Home</li>
             <li>Sections</li>
+            <li>Sections</li>
+
         </ul>
     </div>
   )
