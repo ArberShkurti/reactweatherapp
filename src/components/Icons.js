d702c3f299ad1e0c92ac0aad985ca6e29825954a
Arber Shkurti <arbershkurti@yahoo.com>
2024-04-05 21:36:08 +0200

    cmt

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,35 +1,35 @@
-const Icons = (icon) => {
-    console.log (icon)
-    switch (icon) {
-        case 'Thunderstorm':
-            icon='ikons/11n.png'
-            console.log ('vetetima');
-            break;
-        case 'Drizzle':
-            icon='ikons/10d.png'
-            console.log ('shiiii');
-            break;
-        case 'Rain':
-            icon='ikons/09d.png'
-            console.log ('shi');
-            break;
-        case 'Snow':
-            icon='ikons/13d.png'
-            console.log ('debore');
-            break;
-        case 'Clear':
-            icon='ikons/01d.png'
-            console.log ('kthjellet');
-            break;
-        case 'Clouds':
-            icon='kcons/04d.png'
-            console.log ('re');
-            break;
-        default:
-            icon='ikons/03d.png'
-            console.log ('vranesira');
-    }
-    return icon
-}
+// const Icons = (icon) => {
+//     console.log (icon)
+//     switch (icon) {
+//         case 'Thunderstorm':
+//             icon='ikons/11n.png'
+//             console.log ('vetetima');
+//             break;
+//         case 'Drizzle':
+//             icon='ikons/10d.png'
+//             console.log ('shiiii');
+//             break;
+//         case 'Rain':
+//             icon='ikons/09d.png'
+//             console.log ('shi');
+//             break;
+//         case 'Snow':
+//             icon='ikons/13d.png'
+//             console.log ('debore');
+//             break;
+//         case 'Clear':
+//             icon='ikons/01d.png'
+//             console.log ('kthjellet');
+//             break;
+//         case 'Clouds':
+//             icon='kcons/04d.png'
+//             console.log ('re');
+//             break;
+//         default:
+//             icon='ikons/03d.png'
+//             console.log ('vranesira');
+//     }
+//     return icon
+// }
 
-export default Icons;
+// export default Icons;
